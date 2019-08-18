c37050200375f82abda6a5a5158bc188ec3231ad
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-08-18 13:21:31 +0100

    changes to php mailer

--- mailer.php
+++ mailer.php
@@ -3,7 +3,10 @@
     // http://blog.teamtreehouse.com/create-ajax-contact-form
     // Added input sanitizing to prevent injection
     // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["mailer"])) {
+
+    // }
+    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
